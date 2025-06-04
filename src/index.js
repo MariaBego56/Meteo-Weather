d62ae69f48ede71f7ae5b26cb77f27b6c4bd626b
MariaBego56 <148390279+MariaBego56@users.noreply.github.com>
2025-06-04 19:33:12 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 let city="Sidney";
 let apiKey="79c10854b8bbfdaa4tfa826305864ob5";
-let apiUrl=`https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apikey}&units=${metric}`;
+let apiUrl=`https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
 
 
 function displayTemperature(response){
@@ -9,7 +9,7 @@ function displayTemperature(response){
   let temperature = Math.round(response.data.temperature.current);
   temperatureElement.innerHTML = temperature;
   console.log (response);
-  
+
 }
 
 axios.get(apiUrl).then(displayTemperature);
