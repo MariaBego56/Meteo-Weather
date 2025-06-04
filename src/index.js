667a78701f7f0c5911169983826994de89bcc79e
MariaBego56 <148390279+MariaBego56@users.noreply.github.com>
2025-06-04 21:45:52 +0100

    cleaning html

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
-let city="Sydney";
-let apiKey="79c10854b8bbfdaa4tfa826305864ob5";
-let apiUrl=`https://api.shecodes.io/weather/v1/current?query=${"city"}&key=${"apiKey"}&units=metric`;
+
+let apiKey = "79c10854b8bbfdaa4tfa826305864ob5";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+function search(event) {
+  event.preventDefault();
+  let searchInputElement = document.querySelector(".search-input");
+  let city = searchInputElement.value;
+
+}
 
 
-function displayTemperature(response){
+function displayTemperature(response){ 
 
   let temperatureElement = document.querySelector("#weather-temperature-value");
   let temperature = Math.round(response.data.temperature.current);
   temperatureElement.innerHTML = temperature;
-  console.log (response);
 
+
+  axios.get(apiUrl).then(displayTemperature);
 }
-
-axios.get(apiUrl).then(displayTemperature);
