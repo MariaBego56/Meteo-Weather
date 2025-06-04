dc1ff1bbf4c63359b7f98d1654b5790b685dbfac
MariaBego56 <148390279+MariaBego56@users.noreply.github.com>
2025-06-04 07:14:26 +0100

    create js file and add an axios and a api elements

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
-function searchCity(city){
+let city="Sidney";
+let apiKey="79c10854b8bbfdaa4tfa826305864ob5";
+let apiUrl=`https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apikey}&units=${metric}`;
 
 
-  function searchSubmit(event){
-    event.preventDefault();
-    let searchInput = document.querySelector (.search-input);
-  }
+function displayTemperature(response){
 
+  let temperatureElement = document.querySelector("#weather-temperature-value");
+  temperatureElement.innerHTML = response.data.temperature.current;
 }
+
+axios.get(apiUrl).then(displayTemperature);
