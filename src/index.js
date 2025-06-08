0753366aea2ba8e349d83343907a61665a1261f5
MariaBego56 <148390279+MariaBego56@users.noreply.github.com>
2025-06-08 19:24:14 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -15,18 +15,38 @@ function search(event) {
   axios.get(apiUrl).then(displayTemperature);
 }
 
+function formatDate(timestamp) {
+  let date = new Date(timestamp * 1000); 
+  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  let day = days[date.getDay()];
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
 
-function displayTemperature(response){ 
 
-  let temperatureElement = document.querySelector("#weather-temperature-value");
-  let temperature = Math.round(response.data.temperature.current);
-  temperatureElement.innerHTML = temperature;
-  
-  let cityElement = document.querySelector("#city-name");
-  cityElement.innerHTML = response.data.city; 
- 
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
 }
 
+function displayTemperature(response) {
+  let temperature = Math.round(response.data.temperature.current);
+  document.querySelector("#weather-temperature-value").innerHTML = temperature;
+
+  document.querySelector("#city-name").innerHTML = response.data.city;
+
+  let formattedTime = formatDate(response.data.time);
+
+  document.querySelector(".weather-conditions").innerHTML = `
+    ${formattedTime}, ${response.data.condition.description} <br>
+    Humidity: <span id="humidity">${response.data.temperature.humidity}%,</span>
+    Wind: <span id="wind-speed">${response.data.wind.speed}km/h</span>
+  `;
+}
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", search);
