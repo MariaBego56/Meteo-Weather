92149151bb102114578ae8424c861f82ac31a061
MariaBego56 <148390279+MariaBego56@users.noreply.github.com>
2025-06-08 18:16:28 +0100

    cleaned up code

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ let apiKey = `79c10854b8bbfdaa4tfa826305864ob5`;
 
 function search(event) {
   event.preventDefault();
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   let searchInputElement = document.querySelector(".search-input");
   let city = searchInputElement.value;
 
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+ 
+  axios.get(apiUrl).then(displayTemperature);
 }
 
 
@@ -17,6 +20,6 @@ function displayTemperature(response){
   let temperature = Math.round(response.data.temperature.current);
   temperatureElement.innerHTML = temperature;
 
-
-  axios.get(apiUrl).then(displayTemperature);
 }
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", search);
