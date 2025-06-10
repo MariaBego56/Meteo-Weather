a82acb9777a78211432d79f7f33c2041b022beb1
MariaBego56 <148390279+MariaBego56@users.noreply.github.com>
2025-06-10 21:56:28 +0100

    added definition of geoTime.

--- src/index.js
+++ src/index.js
@@ -53,6 +53,23 @@ function getLocalTimeFromGeoNames(lat, lng, callback) {
       callback(null);
     });
 }
+function formatGeoTime(geoTimeString) {
+  let date = new Date(geoTimeString);
+  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  let day = days[date.getDay()];
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
+}
 
 
 function displayTemperature(response) {
