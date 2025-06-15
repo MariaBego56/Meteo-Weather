13d86a1701eaba72d3751fd026b307bb682dcf7a
MariaBego56 <148390279+MariaBego56@users.noreply.github.com>
2025-06-15 21:10:04 +0100

    add functions to display the dates in forecast

--- src/index.js
+++ src/index.js
@@ -98,7 +98,12 @@ function getForecast(city){
 axios(apiUrl).then(displayForecast);
 
 }
-
+function formatDay(timestamp){
+let time = new Date (timestamp * 1000);
+let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri","Sat" ]
+ 
+return days[date.getDay()];
+}
 function displayForecast (response){
 
 let forecastHtml ="";
@@ -107,7 +112,7 @@ response.data.daily.forEach(function(day, index) {
   if (index <5){
 forecastHtml = forecastHtml + 
 `<div class="weather-forecast-day">
-          <div class="weather-forecast-date">${day}</div>
+          <div class="weather-forecast-date">${formatDay(day.time)}</div>
           <img src="${day.condition.icon_url}" class="weather-forecast-icon"/>
           <div class="weather-forecast-temperatures">
             <div class="weather-forecast-temperature"><strong>${Math.round(response.day.temperature.maximum)}°</strong></div>
